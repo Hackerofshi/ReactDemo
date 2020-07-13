4467920a82de6e515266a47b924afc6be1278329
shixin <shixin217@126.com>
2020-07-13 10:59:37 +0800

    styled demo

--- src/pages/styled/DemoStyled.jsx
+++ src/pages/styled/DemoStyled.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
-  background-color: blue;
-  margin-top: 10px;
-  color: white;
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+// A new component based on Button, but with some override styles
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
 `;
 
 const Container = styled.div`
@@ -12,11 +24,94 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+const Link = ({ className, children }) => (
+  <a className={className}>{children}</a>
+);
+
+const StyledLink = styled(Link)`
+  color: palevioletred;
+  font-weight: bold;
+`;
+
+const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+  color: blue;
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+  & ~ & {
+    background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+  }
+  & + & {
+    background: lime; // <Thing> next to <Thing>
+  }
+  &.something {
+    background: orange; // <Thing> tagged with an additional CSS class ".something"
+  }
+  .something-else & {
+    border: 1px solid; // <Thing> inside another element labeled ".something-else"
+  }
+`;
+
+const Input = styled.input.attrs((props) => ({
+  // we can define static props
+  type: "password",
+
+  // or we can define dynamic ones
+  size: props.size || "1em",
+}))`
+  color: palevioletred;
+  font-size: 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+
+  /* here we use the dynamically computed prop */
+  margin: ${(props) => props.size};
+  padding: ${(props) => props.size};
+`;
+
+// Create the keyframes
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+// Here we create a component that will rotate everything we pass in over two seconds
+const Rotate = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s linear infinite;
+  padding: 2rem 1rem;
+  font-size: 1.2rem;
+`;
+
 export default class DemoStyled extends React.Component {
   render() {
     return (
       <Container>
         <Button>测试</Button>
+        <Button primary>Primary</Button>
+        <TomatoButton>Tomato Button</TomatoButton>
+
+        <Link>Unstyled, boring Link</Link>
+        <br />
+        <StyledLink>Styled, exciting Link</StyledLink>
+
+        <Thing>Hello world!</Thing>
+        <Thing>How ya doing?</Thing>
+        <Thing className="something">The sun is shining...</Thing>
+        <div>Pretty nice day today.</div>
+        <Thing>Don't you think?</Thing>
+        <div className="something-else">
+          <Thing>Splendid.</Thing>
+        </div>
+
+        <div>
+          <Input placeholder="A small text input" />
+          <br />
+          <Input placeholder="A bigger text input" size="2em" />
+        </div>
       </Container>
     );
   }
